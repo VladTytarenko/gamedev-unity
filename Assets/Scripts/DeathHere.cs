bf7ea7a4e29a4a561f9319a3f84b7e84ed9c2f59
Vlad Tytarenko <tytarenkovlad@outlook.com>
2017-05-23 22:22:16 +0300

    HW-2

---
+++ Assets/Scripts/DeathHere.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHere : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter2D (Collider2D collider) {
+        HeroRabit rabit = collider.GetComponent<HeroRabit>();
+
+        if (rabit != null)
+            LevelController.current.onRabitDeath(rabit);
+	}
+}
