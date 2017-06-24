b2a0f48f7f55c23f2bc37257bcfdbf4c4d5c09e3
Vlad Tytarenko <tytarenkovlad@outlook.com>
2017-06-24 13:21:41 +0300

    pr7

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public string level = "LevelMenu";
+
+    public string openLevel = "isOpenAlsway";
+
+    public GameObject lockObj;
+    public GameObject crystObj;
+    public GameObject fruitObj;
+
+    LevelInfo stats;
+
+    bool isOpen = false;
+
+    void Start()
+    {
+        setStats();
+        setIsLocked();
+        showAtributes();
+    }
+
+    void setStats()
+    {
+        string str = PlayerPrefs.GetString(level);
+        stats = JsonUtility.FromJson<LevelInfo>(str);
+        if (stats == null)
+        {
+            stats = new LevelInfo();
+        }
+    }
+
+    void setIsLocked()
+    {
+        string str = PlayerPrefs.GetString(openLevel);
+        LevelInfo openLevelStats = JsonUtility.FromJson<LevelInfo>(str);
+        if (openLevelStats == null)
+        {
+            openLevelStats = new LevelInfo();
+        }
+        isOpen = openLevelStats.levelFinish;
+
+        if (openLevel == "isOpenAlsway")
+            isOpen = true;
+    }
+
+    void showAtributes()
+    {
+        if (stats.levelFinish) Destroy(lockObj);
+        if (!stats.crystals) Destroy(crystObj);
+        if (!stats.fruits) Destroy(fruitObj);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        HeroRabit rabit = collider.GetComponent<HeroRabit>();
+        if (rabit != null && isOpen)
+            SceneManager.LoadScene(level);
+    }
+
+}
---
+++ Assets/Scripts/LevelInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LevelInfo {
+
+    public bool crystals = false;
+    public bool fruits = false;
+    public bool levelFinish = false;
+
+    /*public static bool hasCrystal()
+    {
+        return crystals;
+    }
+
+    public static bool hasFruits()
+    {
+        return fruits;
+    }
+
+    public static bool isLevelFinish()
+    {
+        return levelFinish;
+    }
+
+    public static void setCrystal(bool isHasCrystal)
+    {
+        crystals = isHasCrystal;
+    }
+
+    public static void setFruits(bool isHasFruits)
+    {
+        fruits = isHasFruits;
+    }
+
+    public static void setLevelFinish(bool isLevelFinish)
+    {
+        levelFinish = isLevelFinish;
+    }*/
+}
