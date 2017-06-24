47d38b93befa1bfb67bf0561875a21dbd15eff9d
Vlad Tytarenko <tytarenkovlad@outlook.com>
2017-06-24 19:24:57 +0300

    pr7.3

---
+++ Assets/Scripts/ExitDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ExitDoor : MonoBehaviour
+{
+
+    public GameObject winPrefab;
+
+    public string level = "LevelMenu";
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        HeroRabit rabit = collider.GetComponent<HeroRabit>();
+
+        if (rabit != null)
+        {
+            GameObject parent = UICamera.first.transform.parent.gameObject;
+            NGUITools.AddChild(parent, winPrefab);
+            HeroRabit.currentRabit.enabled = false;
+            Destroy(HeroRabit.currentRabit.GetComponent<Rigidbody2D>());
+        }
+    }
+
+}
