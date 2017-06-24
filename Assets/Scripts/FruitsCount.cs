61abcdb337b9531e65e65153aa588db02ee6467e
Vlad Tytarenko <tytarenkovlad@outlook.com>
2017-06-24 21:39:21 +0300

    pr5.6

--- Assets/Scripts/FruitsCount.cs
+++ Assets/Scripts/FruitsCount.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class FruitsCount : MonoBehaviour {
 
     public static FruitsCount count;
+    public int maxFruitQuantity = 0;
     public static int max = 0;
     UILabel label;
 
 	void Start () {
         count = this;
+        max = maxFruitQuantity;
         label = count.GetComponent<UILabel>();
-        label.text = "0/" + max;
+        label.text = "0/" + maxFruitQuantity;
 	}
 
     public void countFruitsLabel(int number)
     {
-        label.text = number + "/" + max;
+        label.text = number + "/" + maxFruitQuantity;
     }
 	
    
